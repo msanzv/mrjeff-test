2a0645efe4c631a6cc16a773f4d8f34fcc2359ff
Mario Sanz <seiseises@gmail.com>
2018-05-06 20:44:18 +0200

    Added AJAX call for deleting a user

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\User;
 use GuzzleHttp\Client;
@@ -25,5 +26,16 @@ class UsersController extends Controller
         return $this->render('users/list.html.twig', array(
           "users" => $users
         ));
+    }
+
+    /**
+     * @Route("/users-delete/{userId}", name="users_delete")
+     */
+    public function deleteUsersAction(Request $request, $userId)
+    {
+        $client = new Client(['base_uri' => 'https://jsonplaceholder.typicode.com']);
+        $response = $client->delete('users/' . $userId);
+
+        return new JsonResponse(array('data' => $userId));
     }
 }
